35dcc2379bfcef8c20b09b1c51b7b93e79aff4a9
kane2303 <147627917+kane2303@users.noreply.github.com>
2024-11-18 14:46:23 +0700

    Update Recommendation 2

--- src/main/java/org/example/javafxtutorial/RecomUserViewController.java
+++ src/main/java/org/example/javafxtutorial/RecomUserViewController.java
@@ -13,21 +13,34 @@ import logic.Book;
 
 public class RecomUserViewController extends UserViewController {
   @FXML
-  private HBox cardLayout;
+  private HBox cardLayout1;
+  @FXML
+  private HBox cardLayout2;
+  @FXML
+  private HBox cardLayout3;
+  @FXML
+  private HBox cardLayout4;
 
   public void init() {
     try {
       List<Book> books = libraryService.getBooks();
-      for (Book book : books) {
-        FXMLLoader cardLoader = new FXMLLoader(
-            getClass().getResource("/org/example/javafxtutorial/recom-card.fxml"));
-        HBox cardBox = cardLoader.load();
-        BookCardController bookCardController = cardLoader.getController();
-        bookCardController.setBook(book,this);
-        cardLayout.getChildren().add(cardBox);
-      }
+      initializeCardLayout(cardLayout1, books);
+      initializeCardLayout(cardLayout2, books.subList(books.size() / 4, books.size() / 2));
+      initializeCardLayout(cardLayout3, books.subList(books.size() / 2, 3 * books.size() / 4));
+      initializeCardLayout(cardLayout4, books.subList(3 * books.size() / 4, books.size()));
     } catch(IOException e){
       e.printStackTrace();
+    }
+  }
+
+  private void initializeCardLayout(HBox cardLayout, List<Book> books) throws IOException {
+    for (Book book : books) {
+      FXMLLoader cardLoader = new FXMLLoader(
+          getClass().getResource("/org/example/javafxtutorial/recom-card.fxml"));
+      HBox cardBox = cardLoader.load();
+      BookCardController bookCardController = cardLoader.getController();
+      bookCardController.setBook(book, this);
+      cardLayout.getChildren().add(cardBox);
     }
   }
 
